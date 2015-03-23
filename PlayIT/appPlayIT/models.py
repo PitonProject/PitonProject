e231387b17bcfe4f25d84ff8a6c113d256028671
new33ry <ferranverdes@gmail.com>
2015-03-23 23:57:04 +0100

    Primeres taules creades a la DB

--- PlayIT/appPlayIT/models.py
+++ PlayIT/appPlayIT/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-    userName = models.TextField
+    userName = models.TextField(primary_key=True)
+    password = models.TextField()
+    name = models.TextField()
+    surname = models.TextField(null=True)
+    email = models.EmailField()
+
+
+class Pub(models.Model):
+    name = models.TextField()
+    address = models.TextField()
+    city = models.TextField()
+
+class Client_Pub(models.Model):
+    userName = models.ForeignKey(Client, primary_key=True)
+    idPub = models.ForeignKey(Pub, primary_key=True)
