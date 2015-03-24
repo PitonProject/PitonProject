59266b7655b9160d0a7d060ce3a974bbce3a7aef
new33ry <ferranverdes@gmail.com>
2015-03-24 15:02:32 +0100

    Base de dades feta versio 1

--- PlayIT/appPlayIT/models.py
+++ PlayIT/appPlayIT/models.py
@@ -2,18 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-    userName = models.TextField(primary_key=True)
-    password = models.TextField()
+    username = models.TextField(primary_key=True)
+    #password = models.TextField()
     name = models.TextField()
     surname = models.TextField(null=True)
     email = models.EmailField()
 
-
 class Pub(models.Model):
+    id_spotify_user = models.TextField(primary_key=True)
     name = models.TextField()
     address = models.TextField()
     city = models.TextField()
 
 class Client_Pub(models.Model):
-    userName = models.ForeignKey(Client, primary_key=True)
-    idPub = models.ForeignKey(Pub, primary_key=True)
+    id_client = models.ForeignKey(Client, primary_key=True)
+    id_pub = models.ForeignKey(Pub, primary_key=True)
+
+class Playlist(models.Model):
+    id_spotify_playlist = models.TextField(primary_key=True)
+    id_pub = models.ForeignKey(Pub)
+
