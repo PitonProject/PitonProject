068ff74a6a53a9c63c66feb9207596af31cc41fc
new33ry <ferranverdes@gmail.com>
2015-03-24 19:30:46 +0100

    Pantalla de login v1

--- PlayIT/PlayIT/urls.py
+++ PlayIT/PlayIT/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^$', mainpage, name='home'),
 )
