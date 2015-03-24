54c8737fc2b6e2904bdea4296e5964be37e6470a
Marc Sánchez <sanchezfauste@gmail.com>
2015-03-24 19:45:31 +0100

    Afegit api spotify amb metodes basics

---
+++ SpotifyAPI/spotify.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import json
+
+class PlaylistBuilder:
+
+    def __init__(self, user_id, api_key):
+        self.user_id = user_id
+        self.api_key = api_key
+        self.url_base = "https://api.spotify.com/v1/users/" + self.user_id + "/playlists/"
+
+    def create_playlist(self, playlist_name):
+        url = self.url_base
+        data = {}
+        response = self.post_request(url, data)
+
+    def post_request(self, url, data):
+        response = requests.post(url, data=data)
+        return json.loads(response.text)
+
+class SpotifyBrowser:
+
+    url_base = "https://api.spotify.com/v1/search?"
+    url_services = {
+        "track" : "type=track&q=",
+        "album" : "type=album&q=",
+        "playlist" : "type=playlist&q=",
+        "artist" : "type=artist&q="
+    }
+
+    def search_track(self, track):
+        response = self.get_json_response("track", track)
+        tracks = response["tracks"]["items"]
+        for track in tracks:
+            print track["name"], "-", track["artists"][0]["name"], \
+                    "-", track["album"]["name"], "--> id:", track["id"]
+
+    def search_album(self, album):
+        response = self.get_json_response("album", album)
+        albums = response["albums"]["items"]
+        for album in albums:
+            print album["name"], "--> id:", album["id"]
+
+    def search_artist(self, artist):
+        response = self.get_json_response("artist", artist)
+        artists = response["artists"]["items"]
+        for artist in artists:
+            print artist["name"], "--> id:", artist["id"]
+
+    def search_playlist(self, playlist):
+        response = self.get_json_response("playlist", playlist)
+        playlists = response["playlists"]["items"]
+        for playlist in playlists:
+            print playlist["name"], "-", playlist["owner"]["id"], \
+                    "--> id:", playlist["id"]
+
+    def get_json_response(self, service, keywords):
+        url = SpotifyBrowser.url_base \
+                + SpotifyBrowser.url_services[service] \
+                + keywords.replace(" ", "%20")
+        response = requests.get(url)
+        return json.loads(response.text)
+
+if __name__ == '__main__':
+    sb = SpotifyBrowser()
+    #print json.dumps(sb.get_json_response("track", "Som persones"), indent=4)
+    #sb.search_track("Més que la meva sang")
+    #sb.search_artist("Txarango")
+    #sb.search_album("Som riu")
+    sb.search_playlist("Catala")
